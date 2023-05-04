1dca5b767305d47b7fa85b94693e89cfc76101b8
FahimDeveloper <developerfahim246@gmail.com>
2023-05-04 22:31:47 +0600

    Login page design complete

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-
+        <div className='container mx-auto flex justify-center items-center h-screen'>
+            <div className='space-y-6 border border-primary py-10 px-16 rounded-lg'>
+                <h3 className='text-2xl font-medium text-center'>Sign In</h3>
+                <form className='flex flex-col space-y-5'>
+                    <input type="email" placeholder='Email' className='border border-primary px-5 pr-28 py-2 rounded-lg' />
+                    <div>
+                        <input type="password" placeholder='Password' className='border border-primary px-5 pr-28 py-2 rounded-lg' />
+                        <p className='text-end cursor-pointer'>Forgor password</p>
+                    </div>
+                    <button type='submit' className='btn-primary py-1 rounded-lg text-lg'>Sign In</button>
+                    <div className='flex justify-center items-center gap-x-5'>
+                        <hr className='w-2/5' />
+                        <p className='text-lg'>or</p>
+                        <hr className='w-2/5' />
+                    </div>
+                    <div className='space-y-3'>
+                        <Link>
+                            <button className='border border-primary w-full text-center py-1 rounded-lg my-2'>
+                                <i className="fa-brands fa-google"></i> Sign in with Google
+                            </button>
+                        </Link>
+                        <Link>
+                            <button className='border border-primary w-full text-center py-1 rounded-lg my-2'>
+                                <i className="fa-brands fa-github"></i> Sign in with Github
+                            </button>
+                        </Link>
+                        <p className='text-center'>
+                            Don't have an account ? <Link className='text-primary'>Register</Link>
+                        </p>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
